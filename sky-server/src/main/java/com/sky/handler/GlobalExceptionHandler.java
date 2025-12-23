7938ce08bb28eda4a070bf72288803afe0bb2d58
liangach <liangach3942l@outlooke.com>
2025-12-23 20:42:01 +0800

    员工启用禁用功能

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,16 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+       String meesage = ex.getMessage();
+       if(meesage.contains("Duplicate entry")){
+           String[] split = meesage.split(" ");
+           String username = split[2];
+           String msg = username + MessageConstant.ALREADY_EXISTS;
+           return Result.error(msg);
+       } else {
+           return Result.error("未知错误");
+       }
+    }
 }
